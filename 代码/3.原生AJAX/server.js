611059a7a92098b29b83b29fbfdf28000afb6e3a
bao-th <3283706085@qq.com>
2021-10-25 22:47:29 +0800

    JSONP、cors跨域处理

--- 代码/3.原生AJAX/server.js
+++ 代码/3.原生AJAX/server.js
@@ -41,6 +41,25 @@ app.get("/delay", (request, response) => {
   }, 3000);
 });
 
+//JSONP
+app.all("/jsonp-server", (request, response) => {
+  const data = {
+    exist: 1,
+    msg: "用户名已经存在",
+  };
+  let str = JSON.stringify(data);
+  response.end(`handle(${str})`);
+});
+
+//cors
+app.all("/cors-server", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  response.setHeader("Access-Control-Allow-Method", "*");
+  // response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  response.send("hello CORS");
+});
+
 app.listen(8000, () => {
   console.log("服务已启动,8000 端口监听中....");
 });
